6c8976a810a3f83cad4bd6f9e8200bbbf239ab0a
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-07-16 00:10:03 +0200

    add category slider

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import axios from 'axios'
-import { useEffect } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { FaHeart, FaRegHeart} from 'react-icons/fa'
 
 function Category({title, fetchURL}) {
 
   const [movies, setMovies] = useState([])
-  console.log(movies);
+
+  const [like, setLike] = useState(false)
+
 
   useEffect(()=>{
     axios.get(fetchURL)
@@ -19,9 +21,9 @@ function Category({title, fetchURL}) {
 
       <div className='relative flex items-center'>
 
-        <div id={'slider'}>
-          {movies.map((movie, id) => (
-            <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+          {movies.map(movie => (
+            <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2' key={movie?.id}>
 
               <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt="movie"/>
 
@@ -29,6 +31,10 @@ function Category({title, fetchURL}) {
 
                 <p className='whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
                   {movie?.title}
+                </p>
+
+                <p className='absolute top-4 left-4 text-gray-300'>
+                  {like ? <FaHeart /> : <FaRegHeart />}
                 </p>
 
               </div>
