3d077bebe5e5d553c45090b90a623ab08775aa01
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-07-15 20:13:18 +0200

    add home route and apiRequest end points

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import {Routes, Route} from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </>
   );
 }
 
