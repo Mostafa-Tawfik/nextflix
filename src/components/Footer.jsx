1f209a3cc171da8f52e69cde8e1c45853f2e1389
Mostafa-Tawfik <m8ustafa.tawfik@gmail.com>
2022-07-27 16:09:39 +0200

    ready for version 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 function Footer() {
   return (
     <footer className='text-white z-20 flex flex-col justify-center items-center p-8 mt-auto bottom-[100px]'>
-      <p className='z-20 font-["Josefin_Sans"]'>Powered by <a href='https://www.themoviedb.org/documentation/api' target={"_blank"} rel={"noreferrer"} className='font-["Poppins"]'>TMDB</a>
-      </p>
+      <p className='z-20 font-["Josefin_Sans"]'>
+        Powered by&nbsp;
+        <a href='https://www.themoviedb.org/documentation/api' target={"_blank"} rel={"noreferrer"} className='font-["Poppins"]'>
+          <img className='w-[40px] inline-block' 
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg" alt="/" />
+        </a>
+      </p>      
 
       <div className='flex gap-2 text-base md:text-2xl z-20'>
 
